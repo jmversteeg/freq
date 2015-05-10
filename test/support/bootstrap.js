30c97906a1b482cfe1c8eff269d68d8328392c69
jmversteeg <janmerijnversteeg@gmail.com>
2015-05-10 23:46:59 +0200

    Remove unused reference

--- test/support/bootstrap.js
+++ test/support/bootstrap.js
@@ -2,8 +2,6 @@ var chai = require("chai");
 var sinonChai = require("sinon-chai");
 var chaiAsPromised = require("chai-as-promised");
 
-var sinon = require('sinon');
-
 chai.use(chaiAsPromised);
 chai.use(sinonChai);
 
